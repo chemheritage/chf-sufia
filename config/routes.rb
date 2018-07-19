70ec72c2c544f6f5ac262302f6750326578f39bb
Eddie Rubeiz <erubeiz@sciencehistory.org>
2018-07-19 10:10:29 -0400

    Initial commit -- work-by-work conversion.

--- config/routes.rb
+++ config/routes.rb
@@ -147,10 +147,8 @@ Rails.application.routes.draw do
   # redirect to signed s3
   get '/download_redirect/:id/:filename_key' => "downloads#s3_download_redirect", as: :s3_download_redirect
 
-
-  #Just as an experiment:
-  post '/concern/parent/:parentid/file_sets/:filesetid/to_child_work' => 'subwork_conversion#to_child_work', as: :to_child_work
-  post '/works/parent/:parentworkid/child/:childworkid/to_fileset'    => 'subwork_conversion#to_fileset', as: :to_fileset
+  post '/concern/parent/:parentid/file_sets/:filesetid/to_child_work' => 'member_conversion#to_child_work', as: :to_child_work
+  post '/works/parent/:parentworkid/child/:childworkid/to_fileset'    => 'member_conversion#to_fileset', as: :to_fileset
 
   resources :solr_documents, only: [:show], path: '/catalog', controller: 'catalog' do
     concerns :exportable
